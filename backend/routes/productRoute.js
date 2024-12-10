64443c9ebd9d776b5eb1e125ec9fa7a805e2a3bf
abodh5921 <abodh.shah@surepass.io>
2024-12-10 20:05:55 +0530

    auth.js is working Fine

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,9 +2,10 @@
 
 const express = require("express");
 const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
+const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
 
-router.route("/products").get(getAllProducts);
+router.route("/products").get( isAuthenticatedUser,  getAllProducts);
 
 router.route("/product/new").post(createProduct);
 
