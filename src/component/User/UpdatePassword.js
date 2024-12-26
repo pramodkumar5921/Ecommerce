5b31d4e55241c3939c052a5d81ae287ddcb577df
ABODHKUMAR <abodh5921@gmail.com>
2024-12-26 13:11:16 +0530

    Update Password is Working Fine

--- src/component/User/UpdatePassword.js
+++ src/component/User/UpdatePassword.js
@@ -60,7 +60,7 @@ const UpdatePassword = ({history}) => {
           <MetaData title="Change Password" />
           <div className="updatePasswordContainer">
             <div className="updatePasswordBox">
-              <h2 className="updatePasswordHeading">Update Profile</h2>
+              <h2 className="updatePasswordHeading">Update Password</h2>
               <form
                 className="updatePasswordForm"
                 encType="multipart/form-data"
