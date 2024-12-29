ef593963a8298b06f78ab231d2be8a6d7c79e67f
ABODHKUMAR <abodh5921@gmail.com>
2024-12-29 18:19:58 +0530

    AddCart to Card is Working Fine

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -2,16 +2,33 @@ import React, { Fragment } from 'react';
 import "./Cart.css";
 import CartItemCard from "./CartItemCard";
 import { useDispatch, useSelector } from 'react-redux';
+import { addItemsToCart } from './../../actions/cartAction';
 
 
 const Cart = () => {
-  const item={
-    product:"productID",
-    price:200,
-    name:"pramod",
-    quantity:1,
-    image:"https://i.ibb.co/DRST11n/1.webp",
+  const dispatch = useDispatch();
+  const {cartItems} = useSelector((state)=>state.cart);
+
+  const increaseQuantity = (id,quantity,stock)=>{
+      const newQty = quantity+1;
+
+      if(stock<=quantity){
+        return;
+      }
+
+      dispatch(addItemsToCart(id,newQty));
   };
+
+  const decreaseQuantity = (id,quantity)=>{
+    const newQty = quantity-1;
+
+    if(1 >= quantity){
+      return;
+    }
+
+    dispatch(addItemsToCart(id,newQty));
+};
+
   return <Fragment>
     <div className='cartPage'>
         <div className='cartHeader'>
@@ -20,17 +37,19 @@ const Cart = () => {
             <p>Subtotal</p>
         </div>
 
-        <div className='cartContainer'>
+        {cartItems && cartItems.map((item)=>(
+          <div className='cartContainer'>
             <CartItemCard item={item}/>
             <div className='cartInput'>
-              <button>-</button>
+              <button onClick={()=>{decreaseQuantity(item.product,item.quantity)}}>-</button>
               <input type='number' value={item.quantity} readOnly/>
-              <button>+</button>
+              <button onClick={()=>{increaseQuantity(item.product,item.quantity,item.stock)}}>+</button>
             </div>
             <p className='cartSubtotal'>{`₹${
                 item.price * item.quantity
             }`}</p>
         </div>
+        ))}
         
           <div className='cartGrossProfit'>
             <div></div>
